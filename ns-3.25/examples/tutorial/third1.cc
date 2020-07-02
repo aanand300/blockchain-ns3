ae0c45f52daa2bda793c375dcc235a146aacbaa3
Aniket Anand <aniket.anand.cer16@iitbhu.ac.in>
2020-07-02 05:31:34 +0200

    First commit

---
+++ ns-3.25/examples/tutorial/third1.cc
@@ -0,0 +1,178 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+
+// Default Network Topology
+//
+// Number of wifi or csma nodes can be increased up to 250
+//                          |
+//                 Rank 0   |   Rank 1
+// -------------------------|----------------------------
+//   Wifi 10.1.3.0
+//                 AP
+//  *    *    *    *
+//  |    |    |    |
+// n0   n1   n2   n3
+
+
+// Creating just the wifi network topology
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("ThirdScriptExample");
+
+int
+main (int argc, char *argv[])
+{
+  bool verbose = true;
+  uint32_t nWifi = 3;
+  bool tracing = false;
+
+  CommandLine cmd;
+  cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
+  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+  cmd.AddValue ("tracing", "Enable pcap tracing", tracing);
+
+  cmd.Parse (argc,argv);
+
+  // Check for valid number of csma or wifi nodes
+  // 250 should be enough, otherwise IP addresses
+  // soon become an issue
+  if (nWifi > 250)
+    {
+      std::cout << "Too many wifi or csma nodes, no more than 250 each." << std::endl;
+      return 1;
+    }
+
+  if (verbose)
+    {
+      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
+
+  //Create STA nodes
+  NodeContainer wifiStaNodes;
+  wifiStaNodes.Create (nWifi);
+
+  //create a AP node
+  NodeContainer wifiApNode;
+  wifiApNode.Create (1);
+
+  YansWifiChannelHelper channel; //= YansWifiChannelHelper::Default ();
+  channel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  channel.AddPropagationLoss ("ns3::LogDistancePropagationLossModel");
+  YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+
+  phy.SetChannel (channel.Create ()); // until here, we are done with PHY layer
+
+  //now we will setup the MAC layer
+  WifiHelper wifi;
+  //wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
+  //trying to change the data rate
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+                                  "DataMode", StringValue ("OfdmRate9Mbps"));
+
+  WifiMacHelper mac;
+
+  Ssid ssid = Ssid ("ns-3-ssid");
+  mac.SetType ("ns3::StaWifiMac",
+    "Ssid", SsidValue (ssid),
+    "ActiveProbing", BooleanValue (false));
+
+  NetDeviceContainer staDevices;
+  staDevices = wifi.Install (phy, mac, wifiStaNodes);
+
+  mac.SetType ("ns3::ApWifiMac",
+               "Ssid", SsidValue (ssid));
+
+  NetDeviceContainer apDevices;
+  apDevices = wifi.Install (phy, mac, wifiApNode);
+
+  MobilityHelper mobility;
+
+  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+    "MinX", DoubleValue (0.0),
+    "MinY", DoubleValue (0.0),
+    "DeltaX", DoubleValue (5.0),
+    "DeltaY", DoubleValue (10.0),
+    "GridWidth", UintegerValue (3),
+    "LayoutType", StringValue ("RowFirst"));
+
+  //mobility for STA nodes
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+    "Bounds", RectangleValue (Rectangle (-100000, 100000, -100000, 100000)));
+
+  mobility.Install (wifiStaNodes);
+
+  //mobility for AP node (it is static in fact)
+  //mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (wifiApNode);
+
+  //until this point our nodes, devices, and channels are created, and mobility models chosen for WiFi nodes
+
+  //installing internet stack now
+  InternetStackHelper stack;
+  stack.Install (wifiApNode);
+  stack.Install (wifiStaNodes);
+
+  Ipv4AddressHelper address;
+
+  address.SetBase ("10.1.3.0", "255.255.255.0");
+
+  Ipv4InterfaceContainer apInterface;
+  apInterface = address.Assign (apDevices);
+
+  address.Assign (staDevices);
+
+  //echo server setup on the static AP node
+  UdpEchoServerHelper echoServer (9);
+  ApplicationContainer serverApps = echoServer.Install (wifiApNode.Get(0)); //install echo server on 10.1.3.1
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+  //trying to set client node on one of the STA node
+  UdpEchoClientHelper echoClient (apInterface.GetAddress(0), 9); //ipv4 address of server node device
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+  ApplicationContainer clientApps =
+    echoClient.Install (wifiStaNodes.Get (2));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  Simulator::Stop (Seconds (10.0));
+
+  //tracing
+  if (tracing == true)
+  {
+  phy.EnablePcap ("third", apDevices.Get (0));
+  }
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
+}
